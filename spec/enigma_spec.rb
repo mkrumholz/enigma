943dbbc626d5f9419236309d2c153ce81b9ae701
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-22 21:31:59 -0600

    Write tests for Enigma#shift_keys and #shift_offsets

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -11,8 +11,32 @@ describe Enigma do
   end
 
   describe '#encrpyt' do
-    enigma = Enigma.new
+    it 'returns an encrypted version of input string' do
+      enigma = Enigma.new
 
-    expect(engima.encrypt("hello world", "02715", "040895")).to eq "keder ohulw"
+      expect(engima.encrypt("hello world", "02715", "040895")).to eq "keder ohulw"
+    end
+  end
+
+  # describe '#generate_key' do
+  #   enigma = Enigma.new
+  #
+  #   expect(enigma.generate_key.length).to eq 5
+  # end
+
+  describe '#shift_keys' do
+    it 'returns an array of shift Keys' do
+      enigma = Enigma.new
+
+      expect(enigma.shift_keys("02715")).to eq [02, 27, 71, 15]
+    end
+  end
+
+  describe '#shift_offsets' do
+    it 'returns an array of shift Offsets' do
+      enigma = Enigma.new
+
+      expect(enigma.shift_offsets("040895")).to eq [1, 0, 2, 5]
+    end
   end
 end
