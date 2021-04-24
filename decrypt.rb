4afcb3abb24bb4b16042b3e1682c409341a7c227
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-24 16:52:24 -0600

    Create decrypt.rb runner file

---
+++ decrypt.rb
@@ -0,0 +1,13 @@
+require 'date'
+require './lib/enigma'
+require './lib/keyable'
+
+enigma = Enigma.new
+
+message = File.read(ARGV[0])
+
+details = enigma.decrypt(message, ARGV[2], ARGV[3])
+
+File.write(ARGV[1], details[:decryption])
+
+p "Created '#{ARGV[1]}' with the key #{details[:key]} and date #{details[:date]}"
