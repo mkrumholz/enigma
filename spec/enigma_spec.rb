f3be73932654dcd033c161f829088590e6f6762c
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-24 17:42:22 -0600

    Write test for Enigma#crack

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -117,6 +117,19 @@ describe Enigma do
     end
   end
 
+  describe '#crack' do
+    it 'returns a decrypted message without a key' do
+      enigma = Enigma.new
+
+      expected = {
+        decryption: 'vjqtbeaweqihssi',
+        key: '08304',
+        date: '291018'
+      }
+      expect(enigma.crack('vjqtbeaweqihssi', '291018')).to eq expected
+    end
+  end
+
   describe '#encrypt_by_index' do
     it 'returns an array of encrypted letter indexes' do
       enigma = Enigma.new
