23a55abee64947fe381daa84d47edbd0abf049cd
Molly Krumholz <mkkrumholz@gmail.com>
2021-04-26 11:55:32 -0600

    Create runner file for code cracking

---
+++ lib/crack.rb
@@ -0,0 +1,18 @@
+require 'date'
+require './lib/enigma'
+require './lib/keyable'
+
+enigma = Enigma.new
+
+message = File.read(ARGV[0])
+
+details = if ARGV[2].nil?
+            enigma.crack(message)
+          else
+            enigma.crack(message, ARGV[2])
+          end
+
+File.write(ARGV[1], details[:decryption])
+
+p "Created '#{ARGV[1]}' with the cracked key" \
+  " #{details[:key]} and date #{details[:date]}"
